22a6b0d9da43c0de3b6d2726ba073ea10baee7e3
Luis Morillo <luis_cnnvd@hotmail.com>
2021-12-02 20:45:10 +0100

    day 1. Done

--- solutions/day-1.hs
+++ solutions/day-1.hs
@@ -1,5 +1,27 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
+import Data.Foldable (Foldable (fold, foldl'))
+import System.Environment (getArgs)
+
+
+countIncreasses :: [Int] -> Int
+countIncreasses [] = 0
+countIncreasses [x] = 0
+countIncreasses xs = sum $ fromEnum . (> 0) <$> zipWith (flip (-)) xs (tail xs)
+
+slideWindows :: [Int] -> [[Int]]
+slideWindows [] = []
+slideWindows [_] = []
+slideWindows [_,_] = []
+slideWindows input@(x:xs) = take 3 input : slideWindows xs
 
 main :: IO ()
-main = do 
-    putStrLn "hellow aoc"
+main = do
+    [path] <- getArgs
+    content <- readFile path
+    let input = read @Int <$> words content
+    putStrLn  "Solution 1 is: "
+    print $ countIncreasses input
+    putStrLn "Solution 2 is: "
+    print $ countIncreasses . fmap sum . slideWindows $ input
