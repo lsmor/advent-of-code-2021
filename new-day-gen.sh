9869888a5b9de79d3f0d5a5202ac611fcf2abb77
Luis Morillo <luis_cnnvd@hotmail.com>
2021-12-05 15:20:50 +0100

    script modifies stack.yaml

--- new-day-gen.sh
+++ new-day-gen.sh
@@ -5,3 +5,4 @@ d=day-${1}
 echo -e "module Main where\n\nmain = print \"${d}\"" > ./solutions/${d}.hs
 echo > ./inputs/${d}.example
 echo > ./inputs/${d}.input
+echo -e "  ${d}:\n    main: ${d}.hs" >> package.yaml 
